c36be9c3a5c4f7a7c4f0fce015d79101b1f11828
jorge.ortiz.c97@gmail.com <jorge.ortiz.c97@gmail.com>
2020-06-07 19:34:06 -0700

    Projects Added

---
+++ src/components/landingpage.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Grid, Cell} from 'react-mdl';
+
+class LandingPage extends React.Component
+{
+    render()
+    {
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https://i.pinimg.com/originals/a7/0e/16/a70e1675c7bc001f1578aa76bb0a7819.png"
+                            alt="Avatar Image"
+                            className="avatar-img"
+                            />
+                        
+                        <div className="banner-text">
+                            <h1>Junior Web Developer</h1>
+                            <hr />
+                            <p>HTML/CSS | Bootstrap | JavaScript | React | Express</p>
+
+                            <div className="social-links">
+
+                                <a href="https://www.facebook.com/jorge.ortizcanizales/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-facebook-square" aria-hidden="true" />
+                                </a>
+
+                                <a href="https://github.com/Ortiz97" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+                                <a href="https://www.youtube.com/channel/UCNXTZJQBLXctAX6w9i5y7qw?view_as=subscriber" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
---
+++ src/components/projects.js
@@ -0,0 +1,220 @@
+import React from 'react';
+import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton} from 'react-mdl';
+
+class Projects extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories()
+    {
+        if(this.state.activeTab === 0)
+        {
+            return(
+                <div className="projects-grid">
+                    {/*Project #1 */}
+                    
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg) center / cover'}}>React Project #1</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    
+                    {/*Project #2 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg) center / cover'}}>React Project #2</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/*Project #3 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg) center / cover'}}>React Project #3</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            );
+        }
+        else if(this.state.activeTab === 1)
+        {
+            return(
+                <div className="projects-grid">
+                    {/*Project #1 */}
+                    
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://lh3.googleusercontent.com/proxy/ez_mzJDjN15au1XwTqwFKJhn2dcdicvEHNkbyDCD_kvRK-nrQfydw_I4tdskN7AzqxGGjv4nQSPn9Yq0J8d15uv6w9VB4Khst4rS-2J-SuzhURMnfr3bl9Cu0QLF) center / cover'}}>Python Project #1</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    
+                    {/*Project #2 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://lh3.googleusercontent.com/proxy/ez_mzJDjN15au1XwTqwFKJhn2dcdicvEHNkbyDCD_kvRK-nrQfydw_I4tdskN7AzqxGGjv4nQSPn9Yq0J8d15uv6w9VB4Khst4rS-2J-SuzhURMnfr3bl9Cu0QLF) center / cover'}}>Python Project #2</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/*Project #3 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://lh3.googleusercontent.com/proxy/ez_mzJDjN15au1XwTqwFKJhn2dcdicvEHNkbyDCD_kvRK-nrQfydw_I4tdskN7AzqxGGjv4nQSPn9Yq0J8d15uv6w9VB4Khst4rS-2J-SuzhURMnfr3bl9Cu0QLF) center / cover'}}>Python Project #3</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            );
+        }
+        else if(this.state.activeTab === 2)
+        {
+            return(
+                <div className="projects-grid">
+
+                    {/*Project #1 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://www.henkla.se/wp-content/uploads/2019/09/ctraining_resize_md.jpg) center / cover'}}>Python Project #1</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    
+                    {/*Project #2 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://www.henkla.se/wp-content/uploads/2019/09/ctraining_resize_md.jpg) center / cover'}}>Python Project #2</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/*Project #3 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '175px', background: 
+                            'url(https://www.henkla.se/wp-content/uploads/2019/09/ctraining_resize_md.jpg) center / cover'}}>Python Project #3</CardTitle>
+
+                        <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            );
+        }
+    }
+
+    render()
+    {
+        return(
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={ (tabId) => this.setState({activeTab: tabId}) } ripple>
+                    <Tab>React</Tab>
+                    <Tab>Python</Tab>
+                    <Tab>C#</Tab>
+                </Tabs>
+
+                
+                <Grid>
+                    <Cell col={12} >
+                        <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default Projects;
