6d264ae9de90c96548f39eb6be603fe3c20e4823
jorge.ortiz.c97@gmail.com <jorge.ortiz.c97@gmail.com>
2020-06-07 19:38:45 -0700

    Projects Section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navigation, Drawer, Content, Header, Layout } from 'react-mdl';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-big-content">
+        <Layout>
+            <Header className="header-color" title="Jorge Ortiz" scroll>
+                <Navigation>
+                    <Link to="/">Home</Link>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact Me</Link>
+                    <Link to="/aboutme">About Me</Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Jorge Ortiz">
+                <Navigation>
+                    <Link to="/">Home</Link>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact Me</Link>
+                    <Link to="/aboutme">About Me</Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main />
+            </Content>
+        </Layout>
     </div>
   );
 }
